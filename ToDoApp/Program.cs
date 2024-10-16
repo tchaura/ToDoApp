daa8dcf2a2c074c7f590060726f8914db855c249
timchaura <timchaura@gmail.com>
2024-10-16 23:08:57 +0300

    Add styling. Db password is now stored in user-secrets

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Todo}/{action=Index}/{id?}");
 
 app.Run();
